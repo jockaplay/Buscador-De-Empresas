0ef87fe9f986e101ccea671bafba7261a6e92023
JockaPlay <74666954+jockaplay@users.noreply.github.com>
2022-12-22 12:11:21 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 from tkinter import *
 from tkinter.ttk import *
 import runner
+import xlsxwriter
 
 #CNPJ Diego: 40378230
 
 App = Tk()
 App.title("Buscador")
 
+def buscar():
+    retorno = runner
+    retorno.execute(f'{entradaText.get()}')
+    
+    res.configure(text=f'{retorno.datareturn()}')
+    
+    workbook = xlsxwriter.Workbook('Relatório.xlsx') 
+    worksheet = workbook.add_worksheet("Empresas") 
+    scores = []
+    scores.append(retorno.datareturn())
+    scores.append(retorno.datareturn())
+    row = 0
+    col = 0
+    for i, j, k in scores:
+        worksheet.write(row, col, i)
+        worksheet.write(row, col + 1, j)
+        worksheet.write(row, col + 2, k)
+        row += 1
+    workbook.close()
+
 framePrincipal = Frame(App)
 
-nome = Label(framePrincipal, text="Código")
+nome = Label(framePrincipal, text="Carregar Arquivo")
 nome.pack(pady="5", padx="5")
 
 entradaText = Entry(framePrincipal, width='34')
 entradaText.pack(pady="5", padx="5")
 
-butPesquisar = Button(framePrincipal, text="Buscar", width='34', command = lambda:runner.execute(f'{entradaText.get()}'))
+butPesquisar = Button(framePrincipal, text="Buscar", width='34', command=lambda:buscar())
 butPesquisar.pack(pady="5", padx="5")
+
+res = Label(framePrincipal, text="")
+res.pack(pady="5", padx="5")
 
 framePrincipal.pack(padx="10", pady="10")
 
--- runner.py
+++ runner.py
@@ -2,9 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
+data = ""
+
 class execute:
     def __init__(self, text:str):
+        global data
         navegador = webdriver.Chrome()
+        navegador.minimize_window()
         navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action=VisualizarDadosContribuinte.do')
         navegador.find_element(By.XPATH, '//*[@id="idTipoDocumento"]').click()
         navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[1]/td[1]/select/option[4]').click()
@@ -15,12 +20,13 @@ class execute:
         nome = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b')
         empresa = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td')
         cidade = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]')
-        print(f'Razão social - {nome.text}\nEmpresa - {empresa.text}\nCidade - {cidade.text}')
         if cidade.text == 'LIMOEIRO DE ANADIA':
-            print("Local")
+            data = [nome.text, empresa.text, cidade.text]
+
         else:
-            print("Externo")
-        
+            data = [nome.text, empresa.text, cidade.text]
+def datareturn():
+    return data
 if __name__ == "__main__":
     execute
 
