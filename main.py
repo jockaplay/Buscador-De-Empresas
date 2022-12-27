3689c5f1cad5ec6e8cea62bdc59de30cc36a78c1
jockaplay <jocksonmatheus@gmail.com>
2022-12-27 16:03:43 -0300

    interface e funcoes adicionais

--- main.py
+++ main.py
@@ -1,52 +1,88 @@
 from tkinter import *
-from tkinter.ttk import *
-from tkinter import filedialog as dlg
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from tkinter import filedialog as dlg # Carregar arquivo
+import pandas as pd
 import runner
 import xlsxwriter
 
 #CNPJ Diego: 40378230
 
+lista = []
+
+def importar():
+    global lista
+    try:
+        arquivo = dlg.askopenfilename(filetypes=[("Planilha", "*.xlsx")]) # dialog para buscar o xlsx (arquivo excel)
+        arq = pd.read_excel(arquivo, header=None)
+        lista = [i for i in arq[0]] # coluna dos códigos
+        nome.configure(text="Importado com sucesso!", foreground="#00aa10")
+    except:
+        nome.configure(text="Não foi possivel importar\nTente novamente.", foreground="#ff1010", justify=CENTER)
+
 App = Tk()
+App.minsize(240, 130)
+App.geometry("240x130")
+style = ttk.Style("litera")
 App.title("Buscador")
+App.iconbitmap("img\R-_1_.ico")
+img = PhotoImage(file="img\R.png")
+App.iconphoto(False, img, img)
 
-# arquivo = dlg.askopenfile(mode='r')
+# arquivo = dlg.askopenfile(mode='r') # dialog para carregar xlsx (arquivo excel)
+# print(arquivo)
+# arquivo.close()
 
-def buscar():
-    retorno = runner
-    retorno.execute(f'{entradaText.get()}')
+def buscar(codigos:list):
+    try:
+        if codigos != []:
+            retorno = runner
+            if retorno.ok == True:
+                scores = []
+                retorno.execute(codigos)
+                
+                scores = retorno.data
+                #  for i in scores:                              #
+                #      res = Label(framePrincipal, text="")      #   Adiciona uma label falando se a empresa é local ou 
+                #      res.pack(pady="5", padx="5")              #   externa para cada pesquisa.
+                #      res.configure(text=f'{i[3]}')             #
+                    
+                workbook = xlsxwriter.Workbook('Relatório.xlsx')
+                worksheet = workbook.add_worksheet("Empresas")
+                format = workbook.add_format()
+                format.set_align("center")
+                worksheet.set_column(2, 2, 25, format)
+                worksheet.set_column(0, 1, 35)
+                row = 0
+                col = 0
+                for pessoa, empresa, local, l in scores:
+                    worksheet.write(row, col, pessoa)
+                    worksheet.write(row, col + 1, empresa)
+                    worksheet.write(row, col + 2, local)
+                    row += 1
+                workbook.close()
+                nome.configure(text="Concluído!", foreground="#00aa10")
+            else:
+                nome.configure(text="Ocorreu um erro de execução, por favor\nreinicie o programa.", foreground="#ff1010", justify=CENTER)
+        else:
+            nome.configure(text="Primeiro importe os CNPJs.", foreground="#ff1010", justify=CENTER)
+    except:
+        print("Deu ruim")
     
-    #res.configure(text=f'{retorno.datareturn()}')
-    
-    workbook = xlsxwriter.Workbook('Relatório.xlsx') 
-    worksheet = workbook.add_worksheet("Empresas") 
-    scores = []
-    scores.append(retorno.datareturn())
-    scores.append(retorno.datareturn())
-    row = 0
-    col = 0
-    for i, j, k in scores:
-        worksheet.write(row, col, i)
-        worksheet.write(row, col + 1, j)
-        worksheet.write(row, col + 2, k)
-        row += 1
-    
-    workbook.close()
 
 #------------------------Interface----------------------#
 
 framePrincipal = Frame(App)
 
-nome = Label(framePrincipal, text="Carregar Arquivo")
+nome = ttk.Label(framePrincipal, text="Importar Arquivo")
 nome.pack(pady="5", padx="5")
 
-entradaText = Entry(framePrincipal, width='34')
-entradaText.pack(pady="5", padx="5")
+entradaCod = ttk.Button(framePrincipal, text="Importar", width=34, command=importar, bootstyle=(SUCCESS, OUTLINE))
+entradaCod.pack(pady="5", padx="5")
 
-butPesquisar = Button(framePrincipal, text="Buscar", width='34', command=lambda:buscar())
+butPesquisar = ttk.Button(framePrincipal, text="Buscar", width=34, command=lambda:buscar(lista), bootstyle=(SUCCESS, OUTLINE))
 butPesquisar.pack(pady="5", padx="5")
 
-res = Label(framePrincipal, text="")
-#res.pack(pady="5", padx="5")
 
 framePrincipal.pack(padx="10", pady="10")
 
--- runner.py
+++ runner.py
@@ -3,28 +3,45 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-data = ""
+data = []
+ok = True
 
 class execute:
-    def __init__(self, text:str):
+    def acao(self, codes:list):
         global data
-        navegador = webdriver.Chrome()
-        navegador.minimize_window()
-        navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action=VisualizarDadosContribuinte.do')
-        navegador.find_element(By.XPATH, '//*[@id="idTipoDocumento"]').click()
-        navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[1]/td[1]/select/option[4]').click()
-        element = navegador.find_element(By.XPATH, '//*[@id="valor"]')
-        element.send_keys(f'{text}')
-        navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[2]/td/input').click()
-        navegador.find_element(By.XPATH, '//*[@id="item"]/tbody/tr/td[6]/a').click()
-        nome = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b')
-        empresa = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td')
-        cidade = navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]')
-        if cidade.text == 'LIMOEIRO DE ANADIA':
-            data = [nome.text, empresa.text, cidade.text]
+        for text in codes:
+            self.navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action=VisualizarDadosContribuinte.do')
+            self.navegador.find_element(By.XPATH, '//*[@id="idTipoDocumento"]').click()
+            self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[1]/td[1]/select/option[4]').click()
+            element = self.navegador.find_element(By.XPATH, '//*[@id="valor"]')
+            element.send_keys(f'{text}')
+            try:
+                self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[2]/td/input').click()
+                self.navegador.find_element(By.XPATH, '//*[@id="item"]/tbody/tr/td[6]/a').click()
+                nome = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b')
+                empresa = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td')
+                cidade = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]')
+                if cidade.text == 'LIMOEIRO DE ANADIA':
+                    data.append([nome.text, empresa.text, cidade.text, "Local"])
 
-        else:
-            data = [nome.text, empresa.text, cidade.text]
+                else:
+                    data.append([nome.text, empresa.text, cidade.text, "Externo"])
+            except:
+                data.append(["invalid-CNPJ", "invalid-CNPJ", "invalid-CNPJ", "invalid-CNPJ"])
+                
+    def __init__(self, codes:list):
+        global ok
+        ok = True
+        try:
+            self.navegador = webdriver.Firefox()
+            self.navegador.minimize_window()
+            self.acao(codes)
+            self.navegador.close()
+        except:
+            ok = False
+            return None
+        
+        
 def datareturn():
     return data
 if __name__ == "__main__":
