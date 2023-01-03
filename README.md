c444ec69f324c5f613ff9da77896a45dbc3d3a95
jockaplay <jocksonmatheus@gmail.com>
2023-01-03 02:20:18 -0300

    Add options

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # selettoBuscador
-buscador de empresas
+Buscador de empresas via raíz de CNPJ
 
 - [x] Retorna o nome da empresa e local
 - [x] Mais de uma empresa por vez
-- [x] gerar um excel com as empresas
+- [x] gerar uma planílha com as empresas
 - [x] busca em um arquivo excel
+
+| Cidades suportadas |       status       |
+|:------------------:|:------------------:|
+| Limoeiro de Anadia | :white_check_mark: |
+|     Taquarana      |        :x:         |
+|     Arapiraca      |        :x:         |
+| Giral do Ponciano  |        :x:         |
+
+### Pacotes utilizados:
+```
+$ pip list
+
+Package                 Version
+-------------------------------
+openpyxl                3.0.10
+pandas*                 1.5.2
+selenium                4.7.2
+ttkbootstrap            1.10.0
+webdriver-manager       3.8.5
+XlsxWriter              3.0.4
+```
+
+### Como usar
+
+- Ao clicar em importar, selecione um arquivo no formato xlsx contendo, na primeira coluna, a raíz do CNPJ das empresas que deseja procurar
+- Logo após, pressione o botão buscar para que o programa comece a fazer o processo, que pode ser observado no navegador que será aberto pelo programa
+- Quando o programa finalizar, abrirá uma janela de exportação para escolher onde e o nome que vai ser salvo o arquivo gerado.
+
+>Muito simples de se usar, mas qualquer dúvida, pode me mandar nos issues
--- runner.py
+++ runner.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 
 data = []
 ok = True
 
 
-class execute:
-    def acao(self, codes:list):
+class Execute:
+    def acao(self, codes: list, cidades: list):
         global data
         for text in codes:
-            self.navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action=VisualizarDadosContribuinte.do')
+            self.navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action'
+                               '=VisualizarDadosContribuinte.do')
             self.navegador.find_element(By.XPATH, '//*[@id="idTipoDocumento"]').click()
             self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[1]/td[1]/select/option[4]').click()
             element = self.navegador.find_element(By.XPATH, '//*[@id="valor"]')
@@ -19,29 +19,34 @@ class execute:
             try:
                 self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[2]/td/input').click()
                 self.navegador.find_element(By.XPATH, '//*[@id="item"]/tbody/tr/td[6]/a').click()
-                nome = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b')
-                empresa = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td')
-                cidade = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]')
-                if cidade.text == 'LIMOEIRO DE ANADIA':
-                    data.append([nome.text, empresa.text, cidade.text, "Local"])
+                nome = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table['
+                                                             '1]/tbody/tr[4]/td/b')
+                empresa = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table['
+                                                                '1]/tbody/tr[6]/td')
+                cidade = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table['
+                                                               '2]/tbody/tr[2]/td[3]')
+                for i in cidades:
+                    if cidade.text == i:
+                        data.append([nome.text, empresa.text, cidade.text, "Local"])
             except:
-                pass
-                
-                
-    def __init__(self, codes:list):
+                print("CNPJ inválido")
+
+    def __init__(self, codes: list, cidades: list):
         global ok
         ok = True
         try:
             self.navegador = webdriver.Firefox()
             self.navegador.minimize_window()
-            self.acao(codes)
+            self.acao(codes, cidades)
             self.navegador.close()
-        except:
+        finally:
             ok = False
-            return None
-        
-        
+            return
+
+
 def datareturn():
     return data
+
+
 if __name__ == "__main__":
-    __ = execute
+    __ = Execute
