443921cd0fb37cac4626230a3d87d51eb7feba51
jockaplay <jocksonmatheus@gmail.com>
2023-01-04 23:58:36 -0300

    v1.1

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import xlsxwriter
 lista = []
 cidades = []
 
-
 bw = int()
 
 
@@ -17,15 +16,39 @@ def select(data):
     bw = data
 
 
-item = int()
+item = 0
+
+
+def saveFile(alinhamento, scores):
+    save = dlg.asksaveasfilename(filetypes=[("Arquivo Excel", "*.xlsx")])
+    if save:
+        workbook = xlsxwriter.Workbook(f'{save}.xlsx')
+        worksheet = workbook.add_worksheet("Empresas")
+        formatado = workbook.add_format()
+        formatado.set_align("center")
+        worksheet.set_column(2, 4, 25, formatado)
+        worksheet.set_column(0, 1, 35)
+        row = 0
+        col = 0
+        for pessoa, empresa, local, email, text in scores:
+            worksheet.write(row, col, pessoa)
+            worksheet.write(row, col + 1, empresa)
+            worksheet.write(row, col + 2, local)
+            worksheet.write(row, col + 3, email)
+            worksheet.write(row, col + 4, text)
+            row += 1
+        workbook.close()
+        nome.configure(text="Concluído!", foreground="#00aa10", justify=alinhamento)
+    else:
+        nome.configure(text="O arquivo não foi salvo.", foreground="#ff1010", justify=alinhamento)
 
 
 def verificar(a):
     global item
     if a == "CACEAL":
-        item = 0
-    elif a == "Raiz do CNPJ":
         item = 1
+    elif a == "Raiz do CNPJ":
+        item = 0
 
 
 def marcar(mark, city: str):
@@ -48,6 +71,20 @@ def importar():
         nome.configure(text="Não foi possivel importar\nTente novamente.", foreground="#ff1010", justify=tk.LEFT)
 
 
+def processo(codigos, alinhamento):
+    retorno = runner
+    if cidades:
+        retorno.Execute(codigos, cidades, bw, item)
+        scores = retorno.data
+        if scores:
+            saveFile(alinhamento, scores)
+        else:
+            nome.configure(text="Nenhuma empresa encontrada.", foreground="#ff1010", justify=alinhamento)
+        scores.clear()
+    else:
+        nome.configure(text="Selecione pelo menos uma cidade.", foreground="#ff1010", justify=alinhamento)
+    retorno.wipedata()
+
 # - - - Inicializar aplicativo - - - #
 
 
@@ -67,43 +104,7 @@ def buscar(codigos: list):
     try:
         if codigos:
             nome.configure(text="Aguarde...", foreground="#cccc10", justify=alinhamento)
-            retorno = runner
-            if retorno.ok:
-                if cidades:
-                    retorno.Execute(codigos, cidades, bw, item)
-                    scores = retorno.data
-                    if scores:
-                        save = dlg.asksaveasfilename(filetypes=[("Arquivo Excel", "*.xlsx")])
-                        if save:
-                            workbook = xlsxwriter.Workbook(f'{save}')
-                            worksheet = workbook.add_worksheet("Empresas")
-                            formatado = workbook.add_format()
-                            formatado.set_align("center")
-                            worksheet.set_column(2, 2, 25, formatado)
-                            worksheet.set_column(0, 1, 35)
-                            row = 0
-                            col = 0
-                            for pessoa, empresa, local, l in scores:
-                                worksheet.write(row, col, pessoa)
-                                worksheet.write(row, col + 1, empresa)
-                                worksheet.write(row, col + 2, local)
-                                row += 1
-                            workbook.close()
-                            nome.configure(text="Concluído!", foreground="#00aa10", justify=alinhamento)
-                        else:
-                            nome.configure(text="O arquivo não foi salvo.",
-                                           foreground="#ff1010", justify=alinhamento)
-                    else:
-                        nome.configure(text="Nenhuma empresa encontrada.",
-                                       foreground="#ff1010", justify=alinhamento)
-                    scores.clear()
-                else:
-                    nome.configure(text="Selecione pelo menos uma cidade.",
-                                   foreground="#ff1010", justify=alinhamento)
-            else:
-                nome.configure(text="Ocorreu um erro de execução, por favor\nreinicie o programa.",
-                               foreground="#ff1010", justify=alinhamento)
-            retorno.wipedata()
+            processo(codigos, alinhamento)
         else:
             nome.configure(text="Primeiro importe os CNPJs.", foreground="#ff1010", justify=alinhamento)
     except:
@@ -125,19 +126,23 @@ frameSide = tk.Frame(framePrincipal)
 
 ttk.Label(frameSide, text="Cidades:").pack(padx="3", pady=(0, 10))
 # Select City
-limoeiro = ttk.Checkbutton(frameSide, text="Limoeiro de Anadia", command=lambda: marcar(limoeiro, "LIMOEIRO DE ANADIA"), style="success")
+limoeiro = ttk.Checkbutton(frameSide, text="Limoeiro de Anadia", command=lambda: marcar(limoeiro, "LIMOEIRO DE ANADIA"),
+                           style="success")
 limoeiro.pack(pady=(0, 10), padx=5, fill="both")
 limoeiro.invoke()
 limoeiro.invoke()
-giral = ttk.Checkbutton(frameSide, text="Giral do Ponciano", command=lambda: marcar(giral, "GIRAL DO PONCIANO"), style="success")
+giral = ttk.Checkbutton(frameSide, text="Giral do Ponciano", command=lambda: marcar(giral, "GIRAL DO PONCIANO"),
+                        style="success")
 giral.pack(pady=(0, 10), padx=5, fill="both")
 giral.invoke()
 giral.invoke()
-taquarana = ttk.Checkbutton(frameSide, text="Taquarana", command=lambda: marcar(taquarana, "TAQUARANA"), style="success")
+taquarana = ttk.Checkbutton(frameSide, text="Taquarana", command=lambda: marcar(taquarana, "TAQUARANA"),
+                            style="success")
 taquarana.pack(pady=(0, 10), padx=5, fill="both")
 taquarana.invoke()
 taquarana.invoke()
-arapiraca = ttk.Checkbutton(frameSide, text="Arapiraca", command=lambda: marcar(arapiraca, "ARAPIRACA"), style="success")
+arapiraca = ttk.Checkbutton(frameSide, text="Arapiraca", command=lambda: marcar(arapiraca, "ARAPIRACA"),
+                            style="success")
 arapiraca.pack(padx=5, fill="both")
 arapiraca.invoke()
 arapiraca.invoke()
@@ -152,12 +157,13 @@ ttk.Label(framePrincipal, text="Status:").pack(pady=(5, 0))
 nome = ttk.Label(framePrincipal, text="Importar Arquivo", justify=tk.CENTER)
 nome.pack(pady="5", padx="5")
 
-
 frameRadials = ttk.LabelFrame(framePrincipal, text="Navegador")
-browser = ttk.Radiobutton(frameRadials, text="Firefox", variable=bw, command=lambda: select(0), value=0, style="success")
+browser = ttk.Radiobutton(frameRadials, text="Firefox", variable=bw, command=lambda: select(0), value=0,
+                          style="success")
 browser.pack(side=tk.LEFT, padx=(5, 0))
 browser.invoke()
-browser2 = ttk.Radiobutton(frameRadials, text="Chrome", variable=bw, command=lambda: select(1), value=1, style="success")
+browser2 = ttk.Radiobutton(frameRadials, text="Chrome", variable=bw, command=lambda: select(1), value=1,
+                           style="success")
 browser2.pack(side=tk.RIGHT, padx=(0, 5))
 frameRadials.pack(fill="x", ipadx="5", ipady="5", padx=5, pady=(5, 10))
 
--- runner.py
+++ runner.py
@@ -1,50 +1,94 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-data = []
-ok = True
+data = list()
 
 
 class Execute:
-    def acao(self, codes: list, cidades: list, tipo: int):
-        global data
+    def acao(self, codes: list, cidades: list, tipodebusca: int):
+        result = list()
         for text in codes:
-            self.navegador.get('http://cadsinc.sefaz.al.gov.br/ConsultarDadosContribuinte.do?action'
-                               '=VisualizarDadosContribuinte.do')
-            if tipo:
-                self.navegador.find_element(By.XPATH, '//*[@id="idTipoDocumento"]').click()
-                self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[1]/td[1]/select/option[4]').click()
-            element = self.navegador.find_element(By.XPATH, '//*[@id="valor"]')
-            element.send_keys(f'{text}')
-            try:
-                self.navegador.find_element(By.XPATH, '/html/body/form/table/tbody/tr[2]/td/input').click()
-                self.navegador.find_element(By.XPATH, '//*[@id="item"]/tbody/tr/td[6]/a').click()
-                nome = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table['
-                                                             '1]/tbody/tr[4]/td/b')
-                empresa = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table['
-                                                                '1]/tbody/tr[6]/td')
-                cidade = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table['
-                                                               '2]/tbody/tr[2]/td[3]')
-                for i in cidades:
-                    if cidade.text == i:
-                        data.append([nome.text, empresa.text, cidade.text, "Local"])
-            except:
-                print("CNPJ inválido.")
+            # 0 == CNPJ
+            # 1 == CACEAL
+            match tipodebusca:
+                case 0:
+                    for cidade in cidades:
+                        resultado = self.searchCNPJ(text)
+                        if cidade == resultado[2]:
+                            result.append(resultado)
+                case 1:
+                    for cidade in cidades:
+                        resultado = self.searchCACEAL(text)
+                        if cidade == resultado[2]:
+                            result.append(resultado)
+        return result
 
-    def __init__(self, codes: list, cidades: list, brows: int, tipo: int):
-        global ok
-        ok = True
+    def searchCNPJ(self, text):
+        nome, rasaosocial, local, contato1 = "Nome", "Razão social", "Cidade", "Email"
         try:
-            if brows == 0:
+            self.navegador.get(
+                f'https://cadsinc.sefaz.al.gov.br/VisualizarDadosContribuinte.do?opcao=raizcnpj&valor={text}')
+            self.navegador.find_element(By.XPATH, '/html/body/div[2]/table/tbody/tr/td[6]/a').click()
+            try:
+                nome = self.navegador.find_element(By.XPATH,
+                                                   '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b').text
+            except:
+                pass
+            try:
+                rasaosocial = self.navegador.find_element(By.XPATH,
+                                                          '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td').text
+            except:
+                pass
+            try:
+                local = self.navegador.find_element(By.XPATH,
+                                                    '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]').text
+            except:
+                pass
+            try:
+                contato1 = self.navegador.find_element(By.XPATH,
+                                                       '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[3]/tbody/tr[2]/td[3]').text
+            except:
+                pass
+        except:
+            pass
+        return [nome, rasaosocial, local, contato1, text]
+
+    def searchCACEAL(self, text):
+        nome, rasaosocial, local, contato1 = "Nome", "Razão social", "Cidade", "Contato"
+        try:
+            self.navegador.get(f'http://cadsinc.sefaz.al.gov.br/VisualizarDadosContribuinte.do?opcao=caceal&valor={text}')
+            try:
+                nome = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[4]/td/b').text
+            except:
+                pass
+            try:
+                rasaosocial = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[1]/table[1]/tbody/tr[6]/td').text
+            except:
+                pass
+            try:
+                local = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[2]/tbody/tr[2]/td[3]').text
+            except:
+                pass
+            try:
+                contato1 = self.navegador.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/fieldset[2]/table[3]/tbody/tr[2]/td[3]').text
+            except:
+                pass
+        except:
+            pass
+        return [nome, rasaosocial, local, contato1, text]
+
+    def __init__(self, codes: list, cidades: list, witchNav: int, tipo: int):
+        global data
+        try:
+            if witchNav == 0:
                 self.navegador = webdriver.Firefox()
             else:
                 self.navegador = webdriver.Chrome()
             self.navegador.minimize_window()
-            self.acao(codes, cidades, tipo)
+            data = self.acao(codes, cidades, tipo)
             self.navegador.close()
         except:
-            ok = False
-            return
+            pass
 
 
 def wipedata():
